a813079b4352adad594d22be6946fe86261bc04a
Nick Reszetnik <nick.reszetnik@Nick-Reszetnik.local>
2018-11-14 16:40:22 -0500

    project example

---
+++ StringExample.py
@@ -0,0 +1,13 @@
+def second_half(str):
+	return str[0:lstr/2]
+
+#If I wanted to return a substring that goes from a certain index number 
+#to the end of the first string it sould look smething like this...
+
+	return str[2:]
+	return str[5:]
+	return str[8:]
+
+#The 2,5,8 represents where I want the new substring to start from. Since
+#there is no ending point, the substring will go on unitll it reaches 
+#the end of the orginal strings length. 
