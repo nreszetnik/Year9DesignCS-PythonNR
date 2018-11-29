6bda865d54f6964cec0edc07ca4a7f39b8504b90
Nick Reszetnik <nick.reszetnik@Nick-Reszetnik.local>
2018-11-29 12:09:56 -0500

    update to program

--- GUICommodityProgram.py
+++ GUICommodityProgram.py
@@ -3,12 +3,15 @@ import tkinter as tk
 
 def change(*args):
 	print("running change")
+	print(metalType.get())
+	if metalType.get() == "Silver":
+		dropDownMenu.config(background = "#C0C0C0")
 
 root = tk.Tk()
 
-titleLabel = tk.Label(root, text = "Commodity Program", font=("Helvetica", 16))
+titleLabel = tk.Label(root, text = "Commodity Program", font = ("Helvetica", 20))
 
-titleLabel.grid(row = 0, column = 0, columnspan = 2)
+titleLabel.grid(row = 0, column = 0, columnspan = 2, sticky = "NESW")
 
 word1Label = tk.Label(root, text = "Metal Type")
 word1Label.grid(row = 1, column = 0, columnspan = 2, sticky = "NESW" )
@@ -26,16 +29,17 @@ metalType.set(MetalOPTIONS[0])
 metalType.trace("w",change)
 
 dropDownMenu = tk.OptionMenu(root,metalType, MetalOPTIONS[0],MetalOPTIONS[1],MetalOPTIONS[2])
-dropDownMenu.grid(row = 2, column = 0, rowspan = 2,sticky = "NESW")
+dropDownMenu.config(background = "#FFD700")
+dropDownMenu.grid(row = 2, column = 0, rowspan = 2, columnspan = 2, sticky = "NESW")
 
 cbHighContrast = tk.Checkbutton(root,text = "High Contrast")
-cbHighContrast.grid(row = 2, column = 1, sticky = "NESW")
+cbHighContrast.grid(row = 2, column = 2, sticky = "NESW")
 
 cbFontsize= tk.Checkbutton(root,text = "Font Size")
-cbFontsize.grid(row = 3, column = 1, sticky = "NESW")
+cbFontsize.grid(row = 3, column = 2, sticky = "NESW")
 
 word1Label = tk.Label(root, text = "Measurement Type")
-word1Label.grid(row = 4, column = 0, columnspan = 2)
+word1Label.grid(row = 4, column = 0, columnspan = 2, sticky = "NESW")
 
 MeasurementOPTIONS = [
 
@@ -48,20 +52,19 @@ measurementType=tk.StringVar(root)
 measurementType.set(MeasurementOPTIONS[0])
 measurementType.trace("w",change)
 
-dropDownMenu = tk.OptionMenu(root,measurementType, MeasurementOPTIONS[0],MeasurementOPTIONS[1],MeasurementOPTIONS[2])
-dropDownMenu.grid(row = 5, column = 0, columnspan = 2)
+dropDownMenu2 = tk.OptionMenu(root,measurementType, MeasurementOPTIONS[0],MeasurementOPTIONS[1],MeasurementOPTIONS[2])
+dropDownMenu2.grid(row = 5, column = 0, columnspan = 2, sticky = "NESW")
 
 
 word1Label = tk.Label(root, text = "Amount of Money You Have To Spend")
-word1Label.grid(row = 6, column = 0, columnspan = 2)
+word1Label.grid(row = 6, column = 0, columnspan = 2, sticky = "NESW")
 
 root.title("GUI Entry")
 entry1 = tk.Entry(root)
-entry1.grid(row = 7, column = 0, columnspan = 2)
+entry1.grid(row = 7, column = 0, columnspan = 2, sticky = "NESW")
 
-root.title("Output Button")
+root.title("PROGRAM")
 btn1 = tk.Button(root, text = "Submit")
-btn1.grid(row = 8, column = 0, columnspan = 2)
-
+btn1.grid(row = 8, column = 0, columnspan = 2, sticky = "NESW")
 
 root.mainloop()
